f515516b5b029396aba5e165c19e0aa77677ec2c
Manoj Nathwani <manoj_1993@hotmail.com>
2015-10-31 23:09:00 +0000

    mvp

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html', name="turtlea")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
