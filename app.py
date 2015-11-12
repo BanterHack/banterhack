5bea96ade810db3ca94ff7bd6a991bf13e33df5f
Manoj Nathwani <manoj_1993@hotmail.com>
2015-11-12 10:43:18 +0000

    going back a commit, no need for backend

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import os
-import json
 from flask import Flask
 from flask import render_template
 from flask.ext.scss import Scss
 
 app = Flask(__name__)
+
 app.debug = True
+
+
 Scss(app, static_dir='static/css', asset_dir='assets/scss')
-data_folder = os.path.dirname(os.path.abspath(__file__)) + "/data"
+
 
 @app.route('/')
 def hello():
-    return render_template(
-        'index.html',
-        sponsors = json.load(open(os.path.join(data_folder, "sponsors.json"), "r"))
-        )
+    return render_template('index.html', name="turtlea")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
